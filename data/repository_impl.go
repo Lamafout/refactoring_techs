b6708cee4a09cfc8d918ce851fba5aac4ccb70ba
lamafout <dzordzvashington@gmail.com>
2024-12-11 15:09:13 +0300

    Started to fix bugs with tech path

--- data/repository_impl.go
+++ data/repository_impl.go
@@ -70,63 +70,93 @@ func (r *RepositoryImpl) GetListOfTechs() (*[]entities.TechShort, error) {
 }
 
 func (r *RepositoryImpl) GetConcreteTech(id int) (*entities.Tech, error) {
-    query := 
-    `SELECT 
-        t.id, t.name, t.specs, t.perfomance, 
-        a.id AS assignment_id, a.name AS assignment_name,
-        r.id AS resources_id, r.energy, r.water, r.neutralization_and_disposal,
-        c.id AS contacts_id, c.address, c.phone, c.fax, c.site,
-        u.id AS use_cases_id, u.name AS use_cases_name,
-        e.id AS expert_info_id, e.authority_name, e.date, e.conclusion,
-        f.id AS fccw_id, f.name AS fccw_name, f.code AS fccw_code,
-        cw.id AS secondary_waste_id, cw.mass, cw.volume, cw.fccw AS secondary_waste_fccw_id
-    FROM 
-        techs t
-    LEFT JOIN assignments a ON t.assignment = a.id
-    LEFT JOIN resources r ON t.resources = r.id
-    LEFT JOIN contacts c ON t.contacts = c.id
-    LEFT JOIN use_cases u ON t.use_case = u.id
-    LEFT JOIN expert_info e ON t.expert_info = e.id
-    LEFT JOIN fccw f ON t.fccw = f.id
-    LEFT JOIN secondary_waste cw ON t.secondary_waste = cw.id
-    WHERE t.id = ?`
+    query := `
+  SELECT 
+    t.id AS tech_id,
+    t.name AS tech_name,
+    t.assignment,
+    a.name AS assignment_name,
+    t.specs,
+    t.resources,
+    r.energy,
+    r.water,
+    r.neutralization_and_disposal,
+    t.performance,
+    sw.id AS secondary_waste_id,
+    sw.mass,
+    sw.volume,
+    f.name AS fccw_name,
+    cpta.id AS cpta_id,
+    cpta.name AS cpta_name
+  FROM 
+    techs t
+  LEFT JOIN assignments a ON t.assignment = a.id
+  LEFT JOIN resources r ON t.resources = r.id
+  LEFT JOIN secondary_waste sw ON sw.id IN (
+    SELECT sec 
+    FROM sec_in_tech 
+    WHERE tech = t.id
+  )
+  LEFT JOIN fccw f ON sw.fccw = f.id
+  LEFT JOIN cpta_in_tech cit ON cit.tech = t.id
+  LEFT JOIN cpta cpta ON cit.cpta = cpta.id
+  WHERE 
+    t.id = $1;
+  `
 
-    row := r.db.QueryRow(query, id)
+    rows, err := r.db.Query(query, id)
+    if err != nil {
+        log.Println("Error executing query:", err)
+        return nil, err
+    }
+    defer rows.Close()
 
     var tech entities.Tech
-    var assignment entities.Assignment
-    var resources entities.Resources
-    var contacts entities.Contacts
-    var useCases entities.UseCases
-    var expertInfo entities.ExpertInfo
-    var fccw entities.Fccw
-    var secondaryWaste entities.SecondaryWaste
+    var secondaryWastes []entities.SecondaryWaste
+    var cptas []entities.Cpta
 
-    err := row.Scan(
-        &tech.ID, &tech.Name, &tech.Specs, &tech.Perfomance,
-        &assignment.ID, &assignment.Name,
-        &resources.ID, &resources.Energy, &resources.Water, &resources.NeutralizationAndDisposal,
-        &contacts.ID, &contacts.Address, &contacts.Phone, &contacts.Fax, &contacts.Site,
-        &useCases.ID, &useCases.Name,
-        &expertInfo.ID, &expertInfo.AuthorityNameCharacter, &expertInfo.Date, &expertInfo.Conclusion,
-        &fccw.ID, &fccw.Name, &fccw.Code,
-        &secondaryWaste.ID, &secondaryWaste.Mass, &secondaryWaste.Volume, &secondaryWaste.FccwId,
-    )
-    if err != nil {
-        if err == sql.ErrNoRows {
-            log.Println("Tech not found:", id)
-            return nil, nil
+    for rows.Next() {
+        var assignment entities.Assignment
+        var resources entities.Resources
+        var secondaryWaste entities.SecondaryWaste
+        var cpta entities.Cpta
+        var fccwName sql.NullString
+
+        err := rows.Scan(
+            &tech.ID, &tech.Name, &tech.Assignment.ID, &assignment.Name,
+            &tech.Specs, &tech.Resources.ID,
+            &resources.Energy, &resources.Water, &resources.NeutralizationAndDisposal,
+            &secondaryWaste.ID, &secondaryWaste.Mass, &secondaryWaste.Volume,
+            &fccwName,
+            &cpta.ID, &cpta.Name,
+        )
+        if err != nil {
+            log.Println("Error scanning row:", err)
+            return nil, err
         }
-        log.Println(err)
+
+        tech.Assignment = assignment
+        tech.Resources = resources
+
+        if secondaryWaste.ID != 0 {
+            if fccwName.Valid {
+                secondaryWaste.Name = fccwName.String
+            }
+            secondaryWastes = append(secondaryWastes, secondaryWaste)
+        }
+
+        if cpta.ID != 0 {
+            cptas = append(cptas, cpta)
+        }
+    }
+
+    if err = rows.Err(); err != nil {
+        log.Println("Error iterating rows:", err)
         return nil, err
     }
 
-    tech.Assignment = assignment
-    tech.Resources = resources
-    tech.Contacts = contacts
-    tech.UseCases = useCases
-    tech.ExpertInfo = expertInfo
-    tech.Fccw = fccw
+    tech.SecondaryWaste = secondaryWastes
+    tech.Cpta = cptas
 
     return &tech, nil
 }
--- domain/entities/tech.go
+++ domain/entities/tech.go
@@ -7,11 +7,11 @@ type Tech struct {
 	Specs string `json:"specs" bd:"specs"`
 	Resources Resources `json:"resources" bd:"resources"`
 	Perfomance string `json:"perfomance" bd:"perfomance"`
-	SecondaryWaste *[]SecondaryWaste `json:"secondary_waste" bd:"secondary_waste"`
+	SecondaryWaste []SecondaryWaste `json:"secondary_waste" bd:"secondary_waste"`
 	Contacts Contacts `json:"contacts" bd:"contacts"`
 	UseCases UseCases `json:"use_cases_case" bd:"use_cases_case"`
 	ExpertInfo ExpertInfo `json:"expert_info" bd:"expert_info"`
 	Fccw Fccw `json:"fccw" bd:"fccw"`
 	UserContacts Contacts `json:"user_contacts" bd:"user_contacts"`
-	Cpta *[]Cpta `json:"cpta" bd:"cpta"`
+	Cpta []Cpta `json:"cpta" bd:"cpta"`
 }
